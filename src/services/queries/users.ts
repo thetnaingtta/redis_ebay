b294444d63ea35fc2639450bc95076ac83fafed9
fatalthetnaing@gmail.com <fatalthetnaing@gmail.com>
2024-09-09 09:23:18 -0700

    updated likes and users  cache

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { userKey } from '$services/keys';
+import { userKey, usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -12,7 +12,15 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
+
+    const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username)
+
+    if (exists) {
+        throw new Error('Username already exists');
+    }
+
     await client.hSet(userKey(id), serialize(attrs));
+    await client.sAdd(usernamesUniqueKey(), attrs.username);
     return id;
 };
 

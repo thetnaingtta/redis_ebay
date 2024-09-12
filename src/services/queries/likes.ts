a9c325a0524737f0adff138520c7cd5cf1bb23cb
fatalthetnaing@gmail.com <fatalthetnaing@gmail.com>
2024-09-11 19:11:34 -0700

    updating likes in a profile

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,11 +1,16 @@
 import { client } from "$services/redis";
 import { itemKey, userLikesKey } from "$services/keys";
+import { getItems } from "./items";
 
 export const userLikesItem = async (itemId: string, userId: string) => {
     return await client.sIsMember(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+    const ids = await client.sMembers(userLikesKey(userId));
+
+    return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
     const inserted = await client.sAdd(userLikesKey(userId), itemId);
@@ -23,4 +28,9 @@ export const unlikeItem = async (itemId: string, userId: string) => {
    }
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const userOneLikes = await client.sMembers(userLikesKey(userOneId));
+    const userTwoLikes = await client.sMembers(userLikesKey(userTwoId));
+
+    return getItems(userOneLikes.filter((id) => userTwoLikes.includes(id)));
+};

dc5f00a4c352bc4424a3ea76fe6ac01dc68caa83
fatalthetnaing@gmail.com <fatalthetnaing@gmail.com>
2024-09-12 10:53:26 -0700

    Added user signup signin check

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -2,5 +2,5 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const userKey = (userId: string) => `user#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemKey = (itemId: string) => `item#${itemId}`;
-export const usernamesUniqueKey = () => 'usernames:unique';
 export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+export const usernamesKey = () => 'usernames';
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,40 +1,60 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { userKey, usernamesUniqueKey } from '$services/keys';
+import { userKey, usernamesUniqueKey, usernamesKey } from '$services/keys';
+import { attr } from 'svelte/internal';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+	// use the username arg to check userid with username sorted set
+
+	const decimalId = await client.zScore(usernamesKey(), username);
+
+	// make sure we actuall got and ID from the lookup
+	if (!decimalId) {
+		throw new Error('User not found');
+	}
+
+	const id = decimalId.toString(16);
+	const user = await client.hGetAll(userKey(id));
+
+	// deserialize the user object from the hash
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
-    const user = await client.hGetAll(userKey(id));
-    return Object.keys(user).length === 0 ? null : deserialize(id, user);
+	const user = await client.hGetAll(userKey(id));
+	return Object.keys(user).length === 0 ? null : deserialize(id, user);
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-    const id = genId();
+	const exists = await client.zScore(usernamesKey(), attrs.username);
 
-    const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username)
+	if (exists) {
+		throw new Error('Username already exists');
+	}
 
-    if (exists) {
-        throw new Error('Username already exists');
-    }
+	const id = genId();
 
-    await client.hSet(userKey(id), serialize(attrs));
-    await client.sAdd(usernamesUniqueKey(), attrs.username);
-    return id;
+	await client.hSet(userKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
+	return id;
 };
 
 const serialize = (user: CreateUserAttrs) => {
-    return {
-        username: user.username,
-        password: user.password
-    };
-}
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
 
 const deserialize = (id: string, user: { [key: string]: string }) => {
-    return {
-        id,
-        username: user.username,
-        password: user.password
-    };  
-}
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
+};
